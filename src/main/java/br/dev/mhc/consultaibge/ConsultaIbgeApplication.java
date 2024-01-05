0029333dddf789300d4b79715fd0d1205521f6f1
Marcelo Henrique Cezario <marcelocezario@gmail.com>
2024-01-05 08:58:25 -0300

    Initial commit

---
+++ src/main/java/br/dev/mhc/consultaibge/ConsultaIbgeApplication.java
@@ -0,0 +1,13 @@
+package br.dev.mhc.consultaibge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaIbgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaIbgeApplication.class, args);
+	}
+
+}
